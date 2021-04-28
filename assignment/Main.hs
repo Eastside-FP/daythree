423b247f3b4b30cb5831b69cb64ca68976ca6f9a
jonathannister <61847178+jonathannister@users.noreply.github.com>
2021-04-27 22:19:30 -0700

    Update Main.hs

--- assignment/Main.hs
+++ assignment/Main.hs
@@ -5,6 +5,10 @@ import Graphics.Gloss
 -- forward pipeline
 (|>) x f = f x
 
+--I'm not entirely sure how to do parts 3 or 4. Below is my best guess, but I'm not sure how to interpret the errors. Apologies--I will try to start earlier and set up a meeting
+--if I get stuck next time. 
+format seconds = ((String)(seconds / 60) ++ ":" ++ (String)(seconds mod 60))
+
 face = 
   circleSolid 350
 
@@ -18,6 +22,10 @@ hub =
   circleSolid 40 
   |> color orange 
 
+label seconds = 
+    text format(seconds)
+    |> color red
+
 main = 
   animate windowed azure renderer
     where
@@ -25,6 +33,8 @@ main =
       renderer seconds = 
         pictures [
            face,
-           hand seconds,
+           hand (seconds * 30),
+           hand ((seconds * 30) / 60), 
+           label,
            hub
          ]
