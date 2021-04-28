eafb8618ea43c4f3b1cf27f0ea92bf226e5d3e04
TEspelien <tespelien@eastsideprep.org>
2021-04-28 06:39:08 -0700

    assignment done

--- assignment/app/Main.hs
+++ assignment/app/Main.hs
@@ -12,11 +12,19 @@ hand seconds =
   rectangleSolid 5 340  
   |> color yellow         
   |> translate (-2.5) 170 
-  |> rotate (seconds * 6)
+  |> rotate (seconds / 30 * 6)
 
 hub =
   circleSolid 40 
   |> color orange 
+
+time seconds = 
+  text (show $ mod (floor $seconds / 30) 60)
+  |> color red
+  |> scale 0.3 0.3
+  |> rotate (seconds / 30 * (-6)) -- rotates counterclockwise to keep the numbers upright
+  |> translate (-10) 350
+  |> rotate (seconds / 30 * 6)
 
 main = 
   animate windowed azure renderer
@@ -25,6 +33,12 @@ main =
       renderer seconds = 
         pictures [
            face,
-           hand seconds,
+
+           hand $ seconds * 30,
+           hand $ seconds * 30 / 60,
+
+           time $ seconds * 30,
+           time $ seconds * 30 / 60,
+
            hub
          ]
